450b7657f2187170940122b286da68ced4b0e92c
Fernando FR <ferlicio@gmail.com>
2024-03-14 17:41:02 -0300

    implementacao de funcoes de rotina

--- message_services/discord/routine_functions/routine_functions.py
+++ message_services/discord/routine_functions/routine_functions.py
@@ -1,6 +1,6 @@
 from database.database import *
 from settings import *
-import discord
+import discord, asyncio
 from colormath.color_objects import sRGBColor, LabColor
 from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie2000
@@ -66,3 +66,90 @@ async def colorIsAvailable(color:str):
             return False
     return True
     
+def formatEventList(eventList):
+    formattedList = '\n\n'.join(
+    f'''> # {event["event_name"].title()}
+>    **Data**: {event["starting_datetime"].strftime("%d/%m/%Y") if event["starting_datetime"].strftime("%d/%m/%Y") == event["ending_datetime"].strftime("%d/%m/%Y") else f"{event['starting_datetime'].strftime('%d')} a {event['ending_datetime'].strftime('%d/%m/%Y')}"}{" - das "+event["starting_datetime"].strftime("%H:%M")+" às "+event["ending_datetime"].strftime("%H:%M") if event["starting_datetime"] == event["ending_datetime"] else ''}
+>    **Local**: {event["city"]}, {event["state_abbrev"]}
+>    **Endereço**: {event["address"]} ''' + '\n'+
+    '\n'.join(filter(None, [
+        f">    **Chat do evento**: <{event['group_chat_link']}>" if event['group_chat_link']!=None else '',
+        f">    **Site**: <{event['website']}>" if event['website']!=None else '',
+        f'''>    **Preço**: {"Ingressos esgotados" if event['out_of_tickets']
+    else "Vendas encerradas" if event['sales_ended']
+    else "A partir de R$"+str(f"{event['price']:.2f}").replace('.',',') if event['price']!=0 else 'Gratuito'}'''
+        ]))
+        for event in sorted(eventList, key=lambda event: event["starting_datetime"]))
+    return formattedList
+
+def formatSingleEvent(event):
+    embeded_description = f'''**Data**: {event["starting_datetime"].strftime("%d/%m/%Y") if event["starting_datetime"].strftime("%d/%m/%Y") == event["ending_datetime"].strftime("%d/%m/%Y") 
+                                            else f"{event['starting_datetime'].strftime('%d')} a {event['ending_datetime'].strftime('%d/%m/%Y')}"}{" - das "+event["starting_datetime"].strftime("%H:%M")+" às "+event["ending_datetime"].strftime("%H:%M") if event["starting_datetime"].strftime("%d/%m/%Y") == event["ending_datetime"].strftime("%d/%m/%Y") else ''}
+**Local**: {event["city"]}, {event["state_abbrev"]}
+**Endereço**: {event["address"]}'''
+    if event['group_chat_link']!=None: embeded_description += f"""
+**Chat do evento**: <{event['group_chat_link']}>"""
+    if event['website']!=None: embeded_description += f"""
+**Site**: <{event['website']}>""" 
+    embeded_description += f"""
+**Preço**: {"Ingressos esgotados" if event['out_of_tickets']
+else "Vendas encerradas" if event['sales_ended']
+else "De R$"+str(f"{event['price']:.0f}").replace('.',',')+" a "+"R${:,.0f}".format(event['max_price']).replace(",", "x").replace(".", ",").replace("x", ".") if (event['price']!=0 and event['max_price']!=0) 
+else f'R$'+str(f"{event['price']:.0f}").replace('.',',') if (event['max_price']==0 or event['max_price']==event['price']) and event['price']!=0 else 'Gratuito'}"""
+    if event['description']!=None: embeded_description += f"""
+
+{event['description']}
+"""
+    eventEmbeded = discord.Embed(
+        color=discord.Color.blue(),
+        title=event["event_name"].title(),
+        description=embeded_description
+    )
+    if event["logo_url"]!=None:
+        eventEmbeded.set_thumbnail(url=event["logo_url"])
+    else: eventEmbeded.set_author(name='')
+    return eventEmbeded
+
+async def removeTempRoles(bot):
+    mydbAndCursor = startConnection()
+    expiringTempRoles = getExpiringTempRoles(mydbAndCursor[0], DISCORD_GUILD_ID)
+    for TempRole in expiringTempRoles:
+        guild = bot.get_guild(DISCORD_GUILD_ID)
+        member = guild.get_member(TempRole['user_id'])
+        role = guild.get_role(TempRole['role_id'])
+        print(f'{member.name} perdeu o cargo {role.name}')
+        pass
+        if member != None and role != None:
+            await member.remove_roles(role)
+            deleteTempRole(mydbAndCursor[1], TempRole['id'])
+    endConnectionWithCommit(mydbAndCursor)
+    return
+
+async def mentionArtRoles(bot, message:discord.Message):
+    guild = bot.get_guild(DISCORD_GUILD_ID)
+    if (message.author.roles.__contains__(guild.get_role(835120204270534707))
+        and message.channel.type == discord.ChannelType.public_thread
+        and message.channel.parent.id == 1210574829107945513
+        and message.channel.owner == message.author
+        ):
+        availableMentions = ['#YCH', '#Rifa', '#Sorteio', '#Adopt']
+        mentionableRoles = [1217107416701997147,1217109127482834965,1217109253647503411,1217112597565014128]
+        rolesDict = dict(zip(availableMentions, mentionableRoles))
+        answer = ''
+        for mention in availableMentions:
+            if message.content.lower().__contains__(mention.lower()):
+                role = guild.get_role(rolesDict[mention])
+                answer += f'{role.mention} '
+        if answer != '':
+            await asyncio.sleep(2)
+            return await message.channel.send(answer, delete_after=5)
+        
+
+async def secureArtPosts(message:discord.Message):
+    if (message.channel.type == discord.ChannelType.public_thread
+        and message.channel.parent.id == 1210574829107945513
+        and message.channel.owner != message.author
+        ):
+        await message.delete()
+        await message.author.send('Você não tem permissão para enviar mensagens no post de outro artista!')
+    return
