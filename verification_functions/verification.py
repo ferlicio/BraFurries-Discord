c35d21c5d13dfc42460d779b2db4f2dc634c57a8
Fernando FR <ferlicio@gmail.com>
2024-10-04 23:21:04 -0300

    correcao retorno do metodo verificar data

--- verification_functions/verification.py
+++ verification_functions/verification.py
@@ -2,6 +2,6 @@ from datetime import datetime, timedelta
 
 def verifyDate(date:str, dateFormat:str=None):
     try:
-        return datetime.strptime(date, "%d/%m/%Y" if dateFormat==None else dateFormat)
+        return datetime.strptime(date, "%d/%m/%Y" if dateFormat==None else dateFormat).date()
     except ValueError:
         return False
