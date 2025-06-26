18f3e1d3d8656859ba5bcfa4dc620f76f6eea89b
Fernando FR <ferlicio@gmail.com>
2025-06-26 17:15:30 -0300

    correcao de importacao

--- commands/moderation.py
+++ commands/moderation.py
@@ -2,6 +2,7 @@ from core.database import connectToDatabase, assignTempRole, endConnectionWithCo
 from discord import Interaction, Member, Role, commands
 from datetime import timedelta, datetime
 from core.common.timeFunctions import now
+from core.database import getUserInfo
 from typing import Literal
 import discord
 import re
