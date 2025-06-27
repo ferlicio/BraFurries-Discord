ee32924cf0ad106cc139c45587887124a4cd0315
ferlicio-anjun <fernando.ribeiro@anjunexpress.com.br>
2025-06-27 14:44:38 -0300

    correcao de funcoes

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from message_services.discord_service import run_discord_client
 import sys, codecs
+import asyncio
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 chatBot = {
     "name": "Coddy",
 }
 
+async def main():
+    await run_discord_client(chatBot)
 
-run_discord_client(chatBot)
+if __name__ == "__main__":
+    asyncio.run(main())
 
 
 
